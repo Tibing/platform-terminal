087eec0ac69b8b565e98ed31ec6bd6f0a4fefd93
Tibing <nik.poltoratsky@gmail.com>
2019-06-12 08:20:26 +0300

    fix: first example content

--- src/app/1-plain-text/1-plain-text.component.ts
+++ src/app/1-plain-text/1-plain-text.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pt-1-plain-text',
   template: `
-    Does it really work?
+    You're breathtaking, Angular In Depth audience!
   `,
 })
 export class PlainTextComponent {
