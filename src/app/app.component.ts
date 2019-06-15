47d6d78d76309a601e5fa4f29fbcc0aa65a3a18e
Tibing <nik.poltoratsky@gmail.com>
2019-06-15 08:12:08 +0300

    chore(deferred): add descriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'pt-root',
   template: `
-    <!--<pt-1-plain-text></pt-1-plain-text>-->
-    <pt-2-text-in-box></pt-2-text-in-box>
+    <pt-1-plain-text></pt-1-plain-text>
+    <!--<pt-2-text-in-box></pt-2-text-in-box>-->
     <!--<pt-3-dashboard></pt-3-dashboard>-->
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
--- src/app/terminal/adapters/deferred.ts
+++ src/app/terminal/adapters/deferred.ts
@@ -3,12 +3,16 @@ import { Widgets as ContribWidgets } from 'blessed-contrib';
 import { ElementFactory } from '../elements-registry';
 import { Widgets } from 'blessed';
 
+interface DeferredGridChildElementParams {
+  row?: number;
+  col?: number;
+  rowSpan?: number;
+  colSpan?: number;
+}
+
 class DeferredElement {
   parent: ContribWidgets.GridElement;
-  row: number;
-  col: number;
-  rowSpan: number;
-  colSpan: number;
+  props: DeferredGridChildElementParams = {};
   data: any;
 
   element: ContribWidgets.LineElement;
@@ -16,58 +20,46 @@ class DeferredElement {
   appendTo(parent: ContribWidgets.GridElement) {
     this.parent = parent;
   }
-
 }
 
+/**
+ * Since not all elements can be rendered as soon as they are added to the parent element
+ * we need to wait until all required properties assigned and only then we can render an element.
+ * That's we're providing deferredElement here. It intercepts all calls to the element factory,
+ * then, waits until all required properties assigned and only then actually renders an element.
+ * */
 export const deferredElement = (elementFactory: ElementFactory) => {
 
   return (options: { screen: Widgets.Screen }) => {
+
+    // Temp object that contains all properties until element rendered
     const temp = new DeferredElement();
+
+    // Creating a proxy to intercept all cals to the element factory
     return new Proxy(temp, {
       set(target: DeferredElement, p: PropertyKey, value: any, receiver: any): boolean {
+
         if (target.element) {
-          if (p === 'data' && value) {
 
-            if (elementFactory === contrib.line) {
-              target.element.setData(value);
-            } else {
-              target.element.setData.apply(target.element, Array.isArray(value) ? value : [value]);
-            }
-            target.data = value;
-          } else {
-            target[p] = value;
-
-            options.screen.remove(target.element);
-            target.element = create();
-          }
-
-          if (elementFactory === contrib.table) {
-            target.element.focus();
-          }
-
+          // If we already have an element assign props and recreate
+          assignToExistingElement(options.screen, target, elementFactory, p, value);
           return true;
         }
 
-        target[p] = value;
+        if (p === 'parent') {
 
-        if (hasRequiredProps(target) && !target.element) {
-          if (elementFactory === contrib.table && !target['columnWidth']) {
-            return true;
-          }
-          (target.parent as any).cellHeight = 8.333333333333334;
-          (target.parent as any).cellWidth = 8.333333333333334;
-          target.element = create();
-
-          if (elementFactory === contrib.table) {
-            target.element.focus();
-          }
+          // If assigning parent just store it in the separate field
+          target.parent = value;
+          return true;
         }
 
-        function create() {
-          const { parent, element, ...opts } = target;
-          return target.parent.set(+target.row, +target.col, +target.rowSpan, +target.colSpan, elementFactory, {
-            ...opts,
-          });
+        // All the props are stored here
+        target.props[p] = value;
+
+        // In case we have all the required properties and element wasn't created yet
+        // we can start creation process
+        if (hasRequiredProps(target) && !target.element) {
+          renderElement(elementFactory, target);
         }
 
         return true;
@@ -76,8 +68,79 @@ export const deferredElement = (elementFactory: ElementFactory) => {
   };
 };
 
-const hasRequiredProps = (obj: {}) => {
-  return requiredProps.every((prop: string) => obj.hasOwnProperty(prop));
+const assignToExistingElement = (screen: Widgets.Screen, target: DeferredElement, elementFactory: Function, p: PropertyKey, value: any) => {
+  if (p === 'data' && value) {
+
+    // Depending on type of the element it can accept data in different formats.
+    // So, here we have a quick fix.
+    if (elementFactory === contrib.line) {
+
+      // For instance line accepts an array of lines as one param.
+      target.element.setData(value);
+    } else {
+
+      // Meanwhile, other elements require parameters to be spread
+      target.element.setData.apply(target.element, Array.isArray(value) ? value : [value]);
+    }
+
+    // When data loaded to the element we need to store it for the rerendering purpose
+    target.data = value;
+  } else {
+
+    // If assigning not data parameter element has to be recreated.
+    // So, just store a new value
+    target.props[p] = value;
+
+    // Completely remove element from the screen
+    screen.remove(target.element);
+
+    // And create new element with new params
+    target.element = create(target, elementFactory);
+  }
+
+  // It's just a trick for demo :)
+  // Focusing element if it's a table
+  if (elementFactory === contrib.table) {
+    target.element.focus();
+  }
+};
+
+const create = (target: DeferredElement, elementFactory: Function) => {
+  const { parent, props, data } = target;
+  const { row, col, rowSpan, colSpan, ...opts } = props;
+
+  // Creating a new element inside the grid
+  return parent.set(+row, +col, +rowSpan, +colSpan, elementFactory, {
+    ...opts,
+    data,
+  });
+};
+
+const renderElement = (elementFactory: Function, target: DeferredElement) => {
+
+  // Quick fix for demo - can't render table without columnWidth property.
+  if (elementFactory === contrib.table && !target.props['columnWidth']) {
+    return true;
+  }
+
+  // Quick fix for demo - actually, we need to wait until grid will be rendered
+  // and only then assign other params.
+  // But now we're just patching grid with required values.
+  (target.parent as any).cellHeight = 8.333333333333334;
+  (target.parent as any).cellWidth = 8.333333333333334;
+
+  // Actually creating an element
+  target.element = create(target, elementFactory);
+
+  // It's just a trick for demo :)
+  // Focusing element if it's a table
+  if (elementFactory === contrib.table) {
+    target.element.focus();
+  }
+};
+
+const hasRequiredProps = (obj: DeferredElement) => {
+  return requiredProps.every((prop: string) => obj.props.hasOwnProperty(prop));
 };
 
 const requiredProps = [
