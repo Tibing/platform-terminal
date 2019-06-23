0bb6b04c9c00103ae13868d5e0fb2be2dde53958
Tibing <nik.poltoratsky@gmail.com>
2019-06-23 12:29:55 +0300

    fix(build): postinstall script

--- projects/playground/src/app/app.component.ts
+++ projects/playground/src/app/app.component.ts
@@ -4,9 +4,9 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'pl-root',
   template: `
-    <pl-1-plain-text></pl-1-plain-text>
+<!--    <pl-1-plain-text></pl-1-plain-text>-->
     <!--<pl-2-text-in-box></pl-2-text-in-box>-->
-    <!--<pl-3-dashboard></pl-3-dashboard>-->
+    <pl-3-dashboard></pl-3-dashboard>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
